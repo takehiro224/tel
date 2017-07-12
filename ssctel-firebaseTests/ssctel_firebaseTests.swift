94b3e15f0a03fe63a6bb8b545bba86b98f63177a
tkwatanabe <tkwatanabe@shinwart.co.jp>
2017-07-12 11:08:46 +0900

    Initial Commit

---
+++ ssctel-firebaseTests/ssctel_firebaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  ssctel_firebaseTests.swift
+//  ssctel-firebaseTests
+//
+//  Created by tkwatanabe on 2017/07/12.
+//  Copyright © 2017年 tkwatanabe. All rights reserved.
+//
+
+import XCTest
+@testable import ssctel_firebase
+
+class ssctel_firebaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
